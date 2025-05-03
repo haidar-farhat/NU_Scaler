658a0cc0f56a180044d33968b8794bf9e949ac8a
Haidar <haydarfarhatsteam69@gmail.com>
2025-05-03 03:35:52 +0300

    Enhance TestCase by adding RefreshDatabase trait and implementing createApplication method for application bootstrapping

--- @web/server/laravel_app/tests/TestCase.php
+++ @web/server/laravel_app/tests/TestCase.php
@@ -3,8 +3,24 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
+use Illuminate\Contracts\Console\Kernel;
+use Illuminate\Foundation\Testing\RefreshDatabase;
 
 abstract class TestCase extends BaseTestCase
 {
-    // Keep this empty unless specific setup needed for ALL tests
+    use CreatesApplication, RefreshDatabase;
+
+    /**
+     * Creates the application.
+     *
+     * @return \Illuminate\Foundation\Application
+     */
+    public function createApplication()
+    {
+        $app = require __DIR__.'/../bootstrap/app.php';
+
+        $app->make(Kernel::class)->bootstrap();
+
+        return $app;
+    }
 }
