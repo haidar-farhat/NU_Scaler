66c5919a0ec53fdd50a986a7ffd5a74bd7af5fa3
Haidar <haydarfarhatsteam69@gmail.com>
2025-05-03 03:34:13 +0300

    Refactor TestCase by removing CreatesApplication trait and commenting on LazilyRefreshDatabase usage for clarity

--- @web/server/laravel_app/tests/TestCase.php
+++ @web/server/laravel_app/tests/TestCase.php
@@ -3,11 +3,8 @@
 namespace Tests;
 
 use Illuminate\Foundation\Testing\TestCase as BaseTestCase;
-use Illuminate\Foundation\Testing\LazilyRefreshDatabase;
-use Illuminate\Foundation\Testing\Concerns\CreatesApplication;
 
 abstract class TestCase extends BaseTestCase
 {
-    use CreatesApplication;
-    // use LazilyRefreshDatabase;
+    // Keep this empty unless specific setup needed for ALL tests
 }
