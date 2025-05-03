2cdf958c1532a5f12c5549f34678803369265c90
Haidar <haydarfarhatsteam69@gmail.com>
2025-05-03 03:31:20 +0300

    Enhance ReviewFactory by defining the model and adding default state attributes for rating, comment, name, email, user_uuid, created_at, and updated_at.

--- @web/server/laravel_app/database/factories/ReviewFactory.php
+++ @web/server/laravel_app/database/factories/ReviewFactory.php
@@ -3,12 +3,20 @@
 namespace Database\Factories;
 
 use Illuminate\Database\Eloquent\Factories\Factory;
+use App\Models\Review;
 
 /**
  * @extends \Illuminate\Database\Eloquent\Factories\Factory<\App\Models\Review>
  */
 class ReviewFactory extends Factory
 {
+    /**
+     * The name of the factory's corresponding model.
+     *
+     * @var string
+     */
+    protected $model = Review::class;
+
     /**
      * Define the model's default state.
      *
@@ -17,7 +25,13 @@ class ReviewFactory extends Factory
     public function definition(): array
     {
         return [
-            //
+            'rating' => $this->faker->numberBetween(1, 5),
+            'comment' => $this->faker->paragraph(),
+            'name' => $this->faker->name(),
+            'email' => $this->faker->safeEmail(),
+            'user_uuid' => null, // Or link to a User factory if needed
+            'created_at' => now(),
+            'updated_at' => now(),
         ];
     }
 }
