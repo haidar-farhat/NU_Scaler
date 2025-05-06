84ae9ca6522deea5d4f941c120de37bf79a40651
Haidar <haydarfarhatsteam69@gmail.com>
2025-05-06 23:09:52 +0300

    Remove Maatwebsite Excel package from composer.json and app configuration
    Eliminated the Maatwebsite Excel dependency from composer.json and removed its service provider and alias from app.php. This change streamlines the application by removing unused components, ensuring a cleaner and more efficient codebase.

--- @web/server/laravel_app/config/app.php
+++ @web/server/laravel_app/config/app.php
@@ -166,7 +166,6 @@ return [
          * Package Service Providers...
          */
         Laravel\Sanctum\SanctumServiceProvider::class,
-        Maatwebsite\Excel\ExcelServiceProvider::class,
 
         /*
          * Application Service Providers...
@@ -185,7 +184,6 @@ return [
         ...array_filter(
             explode(',', env('APP_ALIASES', ''))
         ),
-        'Excel' => Maatwebsite\Excel\Facades\Excel::class,
     ],
 
 ];
