1d01e9c6c20e453e2b6594107a283b258ce91b47
Haidar <haydarfarhatsteam69@gmail.com>
2025-05-07 17:42:05 +0300

    Remove bindgen integration from build.rs in favor of manual FFI definitions. This change simplifies the build process by eliminating unnecessary complexity and aligns with clean code practices by focusing on direct FFI usage.

--- nu_scaler_core/dlss-sys/build.rs
+++ nu_scaler_core/dlss-sys/build.rs
@@ -7,7 +7,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let streamline_sdk_path = PathBuf::from(
         env::var("NVIDIA_STREAMLINE_SDK_PATH")
-            .unwrap_or_else(|_| r"C:\nvideasdk\bckup\Streamline".to_string()),
+            .unwrap_or_else(|_| r"C:\\nvideasdk\\bckup\\Streamline".to_string()),
     );
 
     let sdk_include_path = streamline_sdk_path.join("include");
@@ -31,26 +31,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     println!("cargo:rustc-link-search=native={}", lib_path.display());
     println!("cargo:rustc-link-lib=sl.interposer");
 
-    // Generate bindings using bindgen
-    // The actual sl.h is included by wrapper.h
-    let bindings = bindgen::Builder::default()
-        .header("wrapper.h") // Processes nu_scaler_core/dlss-sys/wrapper.h
-        // Tell bindgen where to find headers included by wrapper.h (like sl.h)
-        .clang_arg(format!("-I{}", sdk_include_path.display()))
-        // Tell bindgen to parse the headers as C++
-        .clang_args(&["-x", "c++", "-std=c++17"]) // Added C++ mode and standard
-        // Add other necessary clang args if needed, e.g., for defines:
-        // .clang_arg("-DSL_DX12_ENABLED=1") // Check sl_config.h for relevant defines
-        .parse_callbacks(Box::new(bindgen::CargoCallbacks::new()))
-        .generate()
-        .expect("Unable to generate bindings for Streamline SDK");
-
-    // Write the bindings to the $OUT_DIR/bindings.rs file.
-    // This is typically included in src/lib.rs as: include!(concat!(env!("OUT_DIR"), "/bindings.rs"));
-    let out_path = PathBuf::from(env::var("OUT_DIR").unwrap());
-    bindings
-        .write_to_file(out_path.join("bindings.rs")) // Standard name often "bindings.rs"
-        .expect("Couldn't write Streamline bindings!");
+    // Bindgen related code has been removed as we are using manual FFI definitions.
 
     Ok(())
 }
